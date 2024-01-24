1095a13c49f44648f519b2c54a52f32489d3868e
Dogfall514 <109625403+Dogfall514@users.noreply.github.com>
2024-01-24 09:21:44 -0600

    Update LegendaryRelic.java

--- src/main/resources/java/com/doggyrelics/relics/LegendaryRelic.java
+++ src/main/resources/java/com/doggyrelics/relics/LegendaryRelic.java
@@ -1,1 +1,12 @@
+import org.bukkit.entity.Player;
 
+public class LegendaryRelic extends Relic {
+    public LegendaryRelic(double rewardChance) {
+        super(createRelicItem("Legendary Relic", Collections.singletonList("Click to get rewards")), rewardChance);
+    }
+
+    @Override
+    public void activateEffect(Player player, List<String> customRewards) {
+        // Implement legendary relic effect with custom rewards
+    }
+}
