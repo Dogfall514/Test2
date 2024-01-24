f78ba997c5f5c912dfe7a8be646c2d9a591bf10f
Dogfall514 <109625403+Dogfall514@users.noreply.github.com>
2024-01-24 09:21:19 -0600

    Update EpicRelic.java

--- src/main/resources/java/com/doggyrelics/relics/EpicRelic.java
+++ src/main/resources/java/com/doggyrelics/relics/EpicRelic.java
@@ -1,1 +1,12 @@
+import org.bukkit.entity.Player;
 
+public class EpicRelic extends Relic {
+    public EpicRelic(double rewardChance) {
+        super(createRelicItem("Epic Relic", Collections.singletonList("Click to get rewards")), rewardChance);
+    }
+
+    @Override
+    public void activateEffect(Player player, List<String> customRewards) {
+        // Implement epic relic effect with custom rewards
+    }
+}
