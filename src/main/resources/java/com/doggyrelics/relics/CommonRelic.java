d2e55e5a2d22706589c07707c321801fb66ac829
Dogfall514 <109625403+Dogfall514@users.noreply.github.com>
2024-01-24 09:20:45 -0600

    Update CommonRelic.java

--- src/main/resources/java/com/doggyrelics/relics/CommonRelic.java
+++ src/main/resources/java/com/doggyrelics/relics/CommonRelic.java
@@ -1,1 +1,13 @@
+import org.bukkit.entity.Player;
 
+public class CommonRelic extends Relic {
+    public CommonRelic(double rewardChance) {
+        super(createRelicItem("Common Relic", Collections.singletonList("Click to get rewards")), rewardChance);
+    }
+
+    @Override
+    public void activateEffect(Player player, List<String> customRewards) {
+        // Implement common relic effect with custom rewards
+        // You can use customRewards to customize the rewards for this relic
+    }
+}
