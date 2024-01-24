43e152f3e790a284ab7a7176e59630c415dec7b4
Dogfall514 <109625403+Dogfall514@users.noreply.github.com>
2024-01-24 09:20:16 -0600

    Update RareRelic.java

--- src/main/resources/java/com/doggyrelics/relics/RareRelic.java
+++ src/main/resources/java/com/doggyrelics/relics/RareRelic.java
@@ -1,1 +1,12 @@
+import org.bukkit.entity.Player;
 
+public class RareRelic extends Relic {
+    public RareRelic(double rewardChance) {
+        super(createRelicItem("Rare Relic", Collections.singletonList("Click to get rewards")), rewardChance);
+    }
+
+    @Override
+    public void activateEffect(Player player, List<String> customRewards) {
+        // Implement rare relic effect with custom rewards
+    }
+}
